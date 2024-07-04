498d1496fc9fb59f2443f5e6944cba6f54cc0170
Vicky Bhattacharya <vickybhattacharya19@gmail.com>
2024-07-04 19:51:57 +0530

    adding mongodb atlas uri

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
-const express = require("express")
-const server = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-dotenv.config()
-const connectionString = process.env.DATABASE_URI
-require("./models/user.models");
+const express = require("express");
+const server = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
+// Load environment variables
+const connectionString = process.env.DATABASE_URI;
+
+// Connect to MongoDB using Mongoose
 mongoose
   .connect(connectionString)
   .then(() => console.log("DB is connected"))
-  .catch((err) => console.log("Error in Connection ", err));
+  .catch((err) => console.log("Error in Connection", err));
 
-
-server.use(express.json())
+// Middleware
+server.use(express.json());
 server.use(cors());
-const auth = require("./routes/auth.routes")
-const flight = require("./routes/flight.routes")
 
-server.use(auth)
-server.use(flight)
+// Import models
+require("./models/user.models");
 
+// Import routes
+const auth = require("./routes/auth.routes");
+const flight = require("./routes/flight.routes");
 
+// Use routes
+server.use(auth);
+server.use(flight);
 
-const PORT = 5001
-server.listen(PORT, ()=>{
-    console.log(`Server listening on port number : ${PORT}`)
-})
-
+// Start the server
+const PORT = 5001;
+server.listen(PORT, () => {
+  console.log(`Server listening on port number: ${PORT}`);
+});
